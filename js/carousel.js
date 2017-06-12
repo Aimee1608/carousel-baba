0be6d9935279c6e134c4633c2a84138e1033369c
aimee1608 <shuigongqian@sina.com>
2017-06-12 18:09:25 +0800

    地方郭德纲

--- js/carousel.js
+++ js/carousel.js
@@ -1,17 +1,16 @@
 /**
  * Created by jiangqian on 2017/6/5.
  */
-$(function(){
     var Carousel = {
         data:{
-            count:$('.poster-list li').length,//数量
-            totalWidth:parseFloat($('.poster-list').css("width")),//总宽度
-            itemWidth:parseFloat($('.poster-list li').css("width")),//单个宽度
+            count:0,//数量
+            totalWidth:0,//总宽度
+            itemWidth:0,//单个宽度
             margin:50,//外边距
-            posterWidth:parseFloat($('.poster-list li').css("width"))+50,//总共的宽度
-            speed:5,//移动速度
+            posterWidth:0,//总共的宽度
+            speed:15,//移动速度
             timer:null,//移动定时器
-            itemArray:$('.poster-list li'),//子元素数组
+            itemArray:null,//子元素数组
             itemLeft:[],//子元素的左定位
             flag:true,
             catched:false,//是否抓到
@@ -19,11 +18,16 @@ $(function(){
         },
         init:function(){//初始化
             console.log(Carousel.data);
+            Carousel.data.count = $('.poster-list li').length;
+            Carousel.data.totalWidth = parseFloat($('.poster-list').css("width"));
+            Carousel.data.itemWidth = parseFloat($('.poster-list li').css("width"));
+            Carousel.data.posterWidth = Carousel.data.itemWidth + Carousel.data.margin;
+            Carousel.data.itemArray = $('.poster-list li');
             for(var i = 0;i<Carousel.data.itemArray.length;i++){
                 var item = $('.poster-list li')[i];
                 //console.log($('.poster-list li')[i]);
-                $(item).css('left',Carousel.data.posterWidth*i);
-                //$(item).css('transform','translateX('+Carousel.data.posterWidth*i+'px)');
+                //$(item).css('left',Carousel.data.posterWidth*i);
+                $(item).css('transform','translateX('+Carousel.data.posterWidth*i+'px)');
                 Carousel.data.itemLeft.push(Carousel.data.posterWidth*i);
             }
             Carousel.move();
@@ -32,66 +36,61 @@ $(function(){
         },
         move:function(){//移动
             Carousel.data.timer = setInterval(function() {
-                var n=0.5;
+                var n=0.6;
                 for(var i=0;i<Carousel.data.itemArray.length;i++){
-                    if( Carousel.data.itemLeft[i]<=-Carousel.data.posterWidth){
-                        $(Carousel.data.itemArray[i]).css('left',-n+Math.max.apply(null, Carousel.data.itemLeft)+Carousel.data.posterWidth);
-                        //$(Carousel.data.itemArray[i]).css('transform','translateX('+(-n+Math.max.apply(null, Carousel.data.itemLeft)+Carousel.data.posterWidth)+'px)');
+                    if( Carousel.data.itemLeft[i]<=-Carousel.data.posterWidth*5){
+                        //$(Carousel.data.itemArray[i]).css('left',-n+Math.max.apply(null, Carousel.data.itemLeft)+Carousel.data.posterWidth);
+                        $(Carousel.data.itemArray[i]).css('transform','translateX('+(-n+Math.max.apply(null, Carousel.data.itemLeft)+Carousel.data.posterWidth)+'px)');
                         Carousel.data.itemLeft[i] =-n+ Math.max.apply(null, Carousel.data.itemLeft)+Carousel.data.posterWidth;
                     }else{
-                        $(Carousel.data.itemArray[i]).css('left',-n+Carousel.data.itemLeft[i]);
-                        //$(Carousel.data.itemArray[i]).css('transform','translateX('+(-n+Carousel.data.itemLeft[i])+'px)');
+                        //$(Carousel.data.itemArray[i]).css('left',-n+Carousel.data.itemLeft[i]);
+                        $(Carousel.data.itemArray[i]).css('transform','translateX('+(-n+Carousel.data.itemLeft[i])+'px)');
                         Carousel.data.itemLeft[i] = -n+Carousel.data.itemLeft[i];
                     }
+
                     //$(Carousel.data.itemArray[i]).css('left',-n+Carousel.data.itemLeft[i]);
                     //Carousel.data.itemLeft[i] = -n+Carousel.data.itemLeft[i];
                 }
+                //console.log(Carousel.data.itemLeft,Carousel.data.itemWidth,Carousel.data.posterWidth);
+                //console.log(parseFloat($('.poster-list li').css("width")),Carousel.data.itemWidth);
             },Carousel.data.speed);
 
         },
         stop:function(){//抓取
             clearInterval(Carousel.data.timer);
-            $('#stop').hide();
             Carousel.data.timer = null;
             $('.zhua').addClass('zhuadown');
+            $('.zhua').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
+                $('.zhua').addClass('zhuaup');
+            });
             var zleft = parseFloat($('.zhua').css('left'));
             var zwidth = parseFloat($('.zhua').css('width'));
             console.log(zleft);
             for(var i=0;i<Carousel.data.itemLeft.length;i++){
                 var left = Carousel.data.itemLeft[i];
-                console.log(left,i);
+                //console.log(left,i);
                 if(left<=zleft && zleft<=(left+Carousel.data.itemWidth)){
-                    console.log(i);
-                    $(Carousel.data.itemArray[i]).addClass('active');
+                    //console.log(i);
+                    $(Carousel.data.itemArray[i]).addClass('img-active');
                     Carousel.data.catched = true;
                     Carousel.data.result = i;
                 }
             }
             if(Carousel.data.catched){
-                $('.result').show();
-                $('.catched').show();
-                $('.nocatched').hide();
-                $('#choosed').show();
-
+                $('#answer-box img').attr('src','img/'+(Carousel.data.result+1)+'.jpg');
+                $('#answer-box').addClass('answer-box-show');
             }else{
-                $('.result').show();
-                $('.nocatched').show();
-                $('.catched').hide();
-                $('#again').show();
+                $('.again-box').show();
+                $('.zhua').removeClass('zhuadown').removeClass('zhuaup');
             }
         },
         start:function(){//开始
             if(Carousel.data.timer==null){
-                $('#answer').hide();
-                $('#choosed').hide();
-                $('#again').hide();
-                $('#stop').show();
+                $('#answer-box').removeClass('answer-box-show');
+                $('.again-box').hide();
                 Carousel.move();
-                $('.zhua').removeClass('zhuadown');
-                $(Carousel.data.itemArray).removeClass('active');
-                $('.result').hide();
-                $('.catched').hide();
-                $('.nocatched').hide();
+                $('.zhua').removeClass('zhuadown').removeClass('zhuaup');
+                $(Carousel.data.itemArray).removeClass('img-active');
                 Carousel.data.catched = false;
                 Carousel.data.result = null;
             }
@@ -99,25 +98,10 @@ $(function(){
         control:function(){//控制
             $('#stop').click(Carousel.stop);
             $('#start').click(Carousel.start);
-            $('#choosed').click(function(){
-                if(Carousel.data.result!=null){
-                    console.log(Carousel.data.result);
-                    $('#answer img').attr('src','img/'+(Carousel.data.result+1)+'.jpg');
-                    $('#answer').show();
-                }else{
-                    Carousel.move();
-                    $('.zhua').removeClass('zhuadown');
-                    $('#choosed').hide();
-                    $('#stop').show();
-                }
-            });
             $('#again').click(function(){
+                $('.again-box').hide();
                 Carousel.start();
             })
         }
 
     };
-    Carousel.init();
-
-
-});
---
+++ js/style.js
@@ -0,0 +1,300 @@
+//init.js
+
+//var mpenshui = document.getElementById('mpenshui');
+//var mshiguan = document.getElementById('mshiguan');
+//var mjiguanqiang = document.getElementById('mjiguanqiang');
+//var mfenglangsheng = document.getElementById('mfenglangsheng');
+//var mmiaobiao = document.getElementById('mmiaobiao');
+//var myunsankai = document.getElementById('myunsankai');
+//var mdeyu = document.getElementById('mdeyu');
+//var mxishui = document.getElementById('mxishui');
+//var mfang = document.getElementById('mfang');
+//var mriyu = document.getElementById('mriyu');
+
+var musicArray = ['mpenshui','mshiguan','mjiguanqiang','mfenglangsheng','mmiaobiao','myunsankai','mdeyu','mxishui','mfang','mriyu'];
+function initMusic(musicIDArray){
+    if(musicIDArray.length>0){
+        for(var i=0;i<musicIDArray.length;i++){
+            var player =document.getElementById(musicIDArray[i]);
+            player.load();
+        }
+    }
+}
+function ImgLoadingByFile(imgArray,loadPageID,loadTxtID,readyID,musicID,showpageID){
+    if(sessionStorage.getItem("pageloaded")){
+        $('#'+loadTxtID).html('100%');
+        $('#'+loadTxtID).hide();
+        $('#'+readyID).show();
+        $('#'+loadPageID).unbind('click').bind('click',function(){
+            var timer = setTimeout(function(){
+                $('#'+loadPageID).fadeOut(800);
+                $('#'+showpageID).show();
+                Carousel.init();
+                //initMusic(musicArray);
+                var player = document.getElementById(musicID);
+                player.load();
+                player.play();
+                clearTimeout(timer);
+            },500);
+        });
+    }else{
+        var imgLoad = 0;
+        if(imgArray.length>0){
+            var imgTotal = imgArray.length;
+            var percent = 0;
+            var img = [];
+            for(var i = 0;i<imgArray.length;i++){
+                img[i] = new Image();
+                img[i].src=imgArray[i];
+                img[i].onload = function(){
+                    imgLoad++;
+                    percent = parseInt(imgLoad/imgTotal*100);
+                    $('#'+loadTxtID).html(percent+'%');
+                    if(percent>=100){
+                      $('#'+loadTxtID).hide();
+                        $('#'+readyID).show();
+                        $('#'+loadPageID).unbind('click').bind('click',function(){
+                            var timer = setTimeout(function(){
+                                //initMusic(musicArray);
+                                var player = document.getElementById(musicID);
+                                player.play();
+                                if(player.paused){
+                                    player.play();
+                                }
+                                $('#'+loadPageID).fadeOut(800);
+                                $('#'+showpageID).show();
+                                Carousel.init();
+                                clearTimeout(timer);
+                            },500);
+                        });
+                        sessionStorage.setItem("pageloaded", "true");
+
+                    }
+                }
+            }
+        }
+    }
+}
+(function(win){
+    var remCalc = {};
+    var docEl = win.document.documentElement,
+        tid,
+        hasRem = true;
+    hasZoom = true;
+    designWidth = 750;
+    function refresh(){
+        var width = docEl.getBoundingClientRect().width;
+        if(hasRem){
+            var rem = width/10;
+            docEl.style.fontSize = rem + "px";
+            remCalc.rem = rem;
+            var actualSize = parseFloat(window.getComputedStyle(document.documentElement)["font-size"]);
+            if(actualSize!== rem && actualSize>0 && Math.abs(actualSize-rem)>1){
+                var remScaled = rem*rem/actualSize;
+                docEl.style.fontSize = remScaled + "px";
+            }
+        }
+        if(hasZoom){
+            var style = document.getElementById('y_style');
+            if(!style){
+                style = document.createElement('style');
+                style.id = 'y_style';
+            }
+            style.innerHTML = '._z{zoom:'+ width/designWidth + '}';
+            document.getElementsByTagName('head')[0].appendChild(style);
+        }
+    }
+    function dbcRefresh(){
+        clearTimeout(tid);
+        tid = setTimeout(refresh,100);
+    }
+    win.addEventListener("resize",function(){
+        dbcRefresh()
+    },false);
+    win.addEventListener("pageshow",function(e){
+        if(e.persisted){
+            dbcRefresh()
+        }
+    },false);
+    refresh();
+    if(hasRem){
+        remCalc.refresh = refresh;
+        remCalc.rem2px = function(d){
+            var val = parseFloat(d)/this.rem;
+            if(typeof d==="string" && d.match(/px$/)){
+                val+="rem";
+            }
+            return val
+        };
+        win.remCalc = remCalc;
+    }
+})(window);
+//横屏
+function landscape(){
+    //var w = window.innerWidth;
+    //var h = window.innerHeight;
+    var w = window.Utils.windowW();
+    var h = window.Utils.windowH();
+    $("body").css({"width":w,"height":h});
+    $('#page-landscape').css({"width":w,"height":h});
+    //$('#page-portrait').css({"width":w,"height":h});
+    $('#page-landscape').show();
+    $('#page-portrait').hide();
+}
+var firstInit = true;
+
+//竖屏
+function portrait02(){
+    var w = window.Utils.windowW();
+    var h = window.Utils.windowH();
+    $("body").css({"width":w,"height":h});
+    $('#page-portrait').css({"width":w,"height":h});
+    //$('.load-page').css({"width":w,"height":h});
+    $('#page-landscape').hide();
+    $('#page-portrait').show();
+    //初始化加载
+    if(firstInit){
+        var imgFile = [
+            './img/music-close.png',
+            './img/music-open.png'
+        ];
+        ImgLoadingByFile(imgFile,'loadingPage','img-loading-txt','readyGo','musicStar','pageContainer');
+        firstInit = false;
+
+    }
+    //音乐
+    $(".open").click(function(){
+        musicStar.pause();
+        $(this).css("display","none");
+        $(".clock").css("display","block");
+    });
+    $(".clock").click(function(){
+        musicStar.play();
+        $(this).css("display","none");
+        $(".open").css("display","block");
+    });
+
+}
+(function() {
+    "use strict";
+
+    function Utils() {
+    }
+
+    Utils.isWeiXin = function(){
+        return navigator.userAgent.match(/MicroMessenger\/([\d\.]+)/);
+    };
+    Utils.isQQ = function(){
+        return navigator.userAgent.ua.match(/QQ\/([\d\.]+)/);
+    };
+    Utils.isQZone = function(){
+        return navigator.userAgent.ua.indexOf("Qzone/") !== -1;
+    };
+
+    Utils.isIos = function() {
+        return !!navigator.userAgent.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/);
+    };
+    Utils.isIPhone = function() {
+        return navigator.userAgent.indexOf('iPhone') > -1 || u.indexOf('Mac') > -1;
+    };
+    Utils.isIpad = function() {
+        return navigator.userAgent.indexOf('iPad') > -1;
+    };
+    Utils.isAndroid = function() {
+        var u = navigator.userAgent;
+        return navigator.userAgent.indexOf('Android') > -1 || u.indexOf('Linux') > -1;
+    };
+    Utils.isMobile = function() {
+        // var u = navigator.userAgent;
+        return navigator.userAgent.match(/(iPhone|iPod|Android|ios|SymbianOS)/i) != null;
+    };
+
+    // ## 屏幕方向
+    Utils.isPortrait = function() {
+        if (!Utils.isMobile()) {
+            //alert(111);
+            return true;
+
+        }
+        // 安卓版 微信里面 只用判断 width 和 height
+        if (Utils.isAndroid() && Utils.isWeiXin()) {
+            if (Utils.windowW() < Utils.windowH()) {
+                //alert(22);
+                return true;
+
+            } else {
+                //alert(331);
+                return false;
+
+            }
+        }
+        var orientation = window['orientation'];
+        if (orientation||orientation==0) {
+            if (orientation == 90 || orientation == -90) {
+                //alert(4442);
+                return false;
+
+            }else{
+                //alert(555111);
+                return true;
+
+            }
+        } else {
+            if (Utils.windowW() < Utils.windowH()) {
+                //alert(666111);
+                return true;
+
+            } else {
+                //alert(777111);
+                return false;
+
+            }
+        }
+    };
+    // ## jquery 获取 window 的宽度
+    Utils.windowW = function() {
+        // var width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
+        return $(window).width();
+    };
+    // ## jquery 获取 window 的高度
+    Utils.windowH = function() {
+        return $(window).height();
+    };
+    window.Utils = Utils;
+}());
+$(function(){
+    onResize();
+    if (!!navigator.userAgent.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/)) {
+        window.addEventListener("onorientationchange" in window ? "orientationchange" : "resize", onResize, false);
+    }else{
+        window.addEventListener( "resize", onResize, false);
+    }
+});
+
+function  onResize() {
+
+    if(Utils.isPortrait()){
+        if(!!navigator.userAgent.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/)){
+
+            var timer = setTimeout(function(){
+                portrait02();
+
+                clearTimeout(timer);
+            },100);
+        }else{
+            portrait02();
+        }
+    } else {
+        if(!!navigator.userAgent.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/)){
+            var timer = setTimeout(function(){
+                landscape();
+                clearTimeout(timer);
+            },100);
+        }else{
+            landscape();
+        }
+    }
+}
+
+
+
